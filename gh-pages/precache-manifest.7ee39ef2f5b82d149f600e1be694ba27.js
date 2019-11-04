ea30e122469e12f3511dea4b36efb6b12aa1db82
Zurdge <jamesedburdge@gmail.com>
2019-11-04 10:41:46 +0900

    added github page

---
+++ gh-pages/precache-manifest.7ee39ef2f5b82d149f600e1be694ba27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3bc5f1faa654959ec8e8eb8b2a1b377",
+    "url": "/quotemap/index.html"
+  },
+  {
+    "revision": "c61e1967d04d90be9eb6",
+    "url": "/quotemap/static/css/main.82819d56.chunk.css"
+  },
+  {
+    "revision": "f4be4e28ce03d8c017d8",
+    "url": "/quotemap/static/js/2.ff2e903d.chunk.js"
+  },
+  {
+    "revision": "c61e1967d04d90be9eb6",
+    "url": "/quotemap/static/js/main.2ef60710.chunk.js"
+  },
+  {
+    "revision": "7019feb0e0e723363d3e",
+    "url": "/quotemap/static/js/runtime-main.325ba805.js"
+  },
+  {
+    "revision": "ec43f5f79700ab5091814840e430609d",
+    "url": "/quotemap/static/media/clouds.ec43f5f7.jpg"
+  }
+]);
